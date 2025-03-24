aec97cb4e5d4d30245c13c0710c1e5d36d5bcd5d
gabkun <95224438+gabkun@users.noreply.github.com>
2025-03-24 15:37:34 +0800

    done-for-checl

--- api/axiosConfig.js
+++ api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8087', // Replace with your actual backend URL
+  baseURL: 'https://vax-barangay-be.onrender.com/', // Replace with your actual backend URL
 });
 
 export default axiosInstance;
